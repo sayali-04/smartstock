6c2aa4c9efcbcf5bf8cc6987b99bf6dfb4378987
Sayali Pawar <sayalipawar1934@gmail.com>
2026-07-12 11:02:33 +0530

    Initial commit: Spring Boot project setup

---
+++ src/test/java/com/example/smartstock/SmartstockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smartstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartstockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
